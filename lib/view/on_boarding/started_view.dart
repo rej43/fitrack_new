4e1c7932a4e9c259628f799f10d5dd4fdcc2afc0
Retr0-0 <pratikmis14@gmail.com>
2025-06-23 08:24:58 +0545

    Added logo to started_view.dart

--- lib/view/on_boarding/started_view.dart
+++ lib/view/on_boarding/started_view.dart
@@ -10,7 +10,30 @@ class StartedView extends StatefulWidget {
   State<StartedView> createState() => _StartedViewState();
 }
 
-class _StartedViewState extends State<StartedView> {
+class _StartedViewState extends State<StartedView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     var media = MediaQuery.of(context).size;
@@ -32,6 +55,15 @@ class _StartedViewState extends State<StartedView> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Spacer(),
+                RotationTransition(
+                  turns: _animation,
+                  child: Image.asset(
+                    "assets/img/FiTrackTransp.png",
+                    width: media.width * 0.4,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                const SizedBox(height: 20),
                 Text(
                   "FiTrack",
                   style: TextStyle(
@@ -52,9 +84,7 @@ class _StartedViewState extends State<StartedView> {
                     padding: const EdgeInsets.symmetric(horizontal: 15),
                     child: RoundButtton(
                       title: "Get Started",
-
                       onPressed: () {
-                        //GO Next Screen
                         Navigator.push(
                           context,
                           MaterialPageRoute(
